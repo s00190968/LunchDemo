837c80707081afcf04c192a9965bb3ea46ef67a2
Janessa Aulén <43206084+s00190968@users.noreply.github.com>
2018-10-10 17:47:07 +0100

    Add files via upload
    foop lab4

---
+++ LunchDemon/LunchDemon/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchDemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lunch l1 = new Lunch("hamburger", "fries", "cola");
+            Lunch l2 = new Lunch("hot dog", "chips", "lemonade");
+            Lunch l3 = new Lunch("pizza", "salad", "iced tea");
+            Lunch l4 = new Lunch("tuna sandwich", "fruit cup", "water");
+            Lunch l5 = new Lunch("peanut butter sandwich", "cookie", "milk");
+
+            Lunch[] lunchArr = new Lunch[3];
+            lunchArr[0] = l1;
+            lunchArr[1] = l2;
+            lunchArr[2] = l3;
+            l1.displayLunches(lunchArr);
+
+            lunchArr = new Lunch[4];
+            lunchArr[0] = l1;
+            lunchArr[1] = l2;
+            lunchArr[2] = l3;
+            lunchArr[3] = l4;
+            l1.displayLunches(lunchArr);
+
+            lunchArr = new Lunch[5];
+            lunchArr[0] = l1;
+            lunchArr[1] = l2;
+            lunchArr[2] = l3;
+            lunchArr[3] = l4;
+            lunchArr[4] = l5;
+            l1.displayLunches(lunchArr);
+        }
+    }
+
+    class Lunch
+    {
+        public string entree { get; set; }
+        public string side { get; set; }
+        public string drink { get; set; }
+
+        public Lunch(string entree, string side, string drink)
+        {
+            this.entree = entree;
+            this.side = side;
+            this.drink = drink;
+        }
+
+        public void displayLunches(params Lunch[] arr)
+        {
+            Console.WriteLine(String.Format("{0,-30} {1,-30} {2,-30}", "Entree:", "Side:", "Drink:"));
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(String.Format("{0,-30} {1,-30} {2,-30}", arr[i].entree, arr[i].side, arr[i].drink));
+            }
+            Console.WriteLine("\n");
+        }
+    }
+    
+}
